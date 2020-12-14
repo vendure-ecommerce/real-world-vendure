d712d78977b193a95489fc8a270b541810a902b4
Michael Bromley <michael@michaelbromley.co.uk>
2020-12-14 13:49:13 +0100

    Add example widgets

--- src/plugins/reviews/ui/reviews-shared.module.ts
+++ src/plugins/reviews/ui/reviews-shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@vendure/admin-ui/core';
 
 import { StarRatingComponent } from './components/star-rating/star-rating.component';
+import { ReviewStateLabelComponent } from './components/review-state-label/review-state-label.component';
 
 @NgModule({
-    imports: [SharedModule, SharedModule],
-    declarations: [StarRatingComponent],
-    exports: [StarRatingComponent, SharedModule],
+    imports: [SharedModule],
+    declarations: [StarRatingComponent, ReviewStateLabelComponent],
+    exports: [StarRatingComponent, ReviewStateLabelComponent, SharedModule],
 })
 export class ReviewsSharedModule {}
--- src/plugins/reviews/ui/reviews-ui-extension.module.ts
+++ src/plugins/reviews/ui/reviews-ui-extension.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
-import { addNavMenuItem, registerCustomFieldComponent } from '@vendure/admin-ui/core';
+import {
+    addNavMenuItem,
+    registerCustomFieldComponent,
+    registerDashboardWidget,
+    setDashboardWidgetLayout,
+} from '@vendure/admin-ui/core';
+import { DEFAULT_DASHBOARD_WIDGET_LAYOUT } from '@vendure/admin-ui/dashboard';
 
 import { ReviewCountLinkComponent } from './components/review-count-link/review-count-link.component';
 import { StarRatingComponent } from './components/star-rating/star-rating.component';
@@ -20,6 +26,23 @@ import { ReviewsSharedModule } from './reviews-shared.module';
             },
             'marketing',
         ),
+        registerDashboardWidget('reviews', {
+            title: 'Latest reviews',
+            supportedWidths: [4, 6, 8, 12],
+            loadComponent: () =>
+                import('./widgets/reviews-widget/reviews-widget.component').then(
+                    m => m.ReviewsWidgetComponent,
+                ),
+        }),
+        registerDashboardWidget('chart', {
+            title: 'Some random chart',
+            supportedWidths: [6, 8, 12],
+            loadComponent: () =>
+                import('./widgets/chart-widget/chart-widget.component').then(
+                    m => m.ChartWidgetComponent,
+                ),
+        }),
+        setDashboardWidgetLayout()
     ],
     exports: [],
 })
