a1e0a5a94f2227f1583a1873a1b7408e1c00f3cc
Artcoded <mail.artcoded@gmail.com>
2021-09-03 11:35:58 +0200

    fixed missing merchant account id (#24)

--- src/plugins/braintree/braintree-payment-method.ts
+++ src/plugins/braintree/braintree-payment-method.ts
@@ -1,79 +1,107 @@
-import { LanguageCode } from '@vendure/common/lib/generated-types';
-import { Logger, PaymentMethodHandler } from '@vendure/core';
+import { LanguageCode } from "@vendure/common/lib/generated-types";
+import {
+  Logger,
+  PaymentMethodHandler,
+  CreatePaymentResult,
+} from "@vendure/core";
 
-import { extractMetadataFromTransaction, getGateway } from './braintree-common';
-import { loggerCtx } from './constants';
+import { extractMetadataFromTransaction, getGateway } from "./braintree-common";
+import { loggerCtx } from "./constants";
 
 /**
  * The handler for Braintree payments.
  */
 export const braintreePaymentMethodHandler = new PaymentMethodHandler({
-    code: 'braintree',
-    description: [{ languageCode: LanguageCode.en, value: 'Braintree' }],
-    args: {
-        merchantId: { type: 'string' },
-        publicKey: { type: 'string' },
-        privateKey: { type: 'string' },
-    },
+  code: "braintree",
+  description: [
+    { languageCode: LanguageCode.en, value: "Braintree" },
+    { languageCode: LanguageCode.it, value: "Braintree" },
+  ],
+  args: {
+    merchantId: { type: "string" },
+    publicKey: { type: "string" },
+    privateKey: { type: "string" },
+    merchantAccountId: { type: "string" },
+    live: { type: "boolean" },
+  },
 
-    async createPayment(ctx, order, amount, args, metadata) {
-        const gateway = getGateway(args);
-        try {
-            const response = await gateway.transaction.sale({
-                amount: (amount / 100).toString(10),
-                orderId: order.code,
-                paymentMethodNonce: metadata.nonce,
-                options: {
-                    submitForSettlement: true,
-                },
-            });
-            if (!response.success) {
-                return {
-                    amount: amount,
-                    state: 'Declined' as const,
-                    transactionId: response.transaction.id,
-                    errorMessage: response.message,
-                    metadata: extractMetadataFromTransaction(response.transaction),
-                };
-            }
-            return {
-                amount: amount,
-                state: 'Settled' as const,
-                transactionId: response.transaction.id,
-                metadata: extractMetadataFromTransaction(response.transaction),
-            };
-        } catch (e) {
-            Logger.error(e, loggerCtx);
-            return {
-                amount: order.total,
-                state: 'Error' as const,
-                transactionId: '',
-                errorMessage: e.toString(),
-                metadata: e,
-            };
-        }
-    },
-
-    settlePayment() {
+  createPayment: async (
+    ctx,
+    order,
+    amount,
+    args,
+    metadata
+  ): Promise<CreatePaymentResult> => {
+    const gateway = getGateway(args);
+    try {
+      const response = await gateway.transaction.sale({
+        amount: (order.totalWithTax / 100).toString(10),
+        orderId: order.code,
+        paymentMethodNonce: metadata.nonce,
+        options: {
+          submitForSettlement: false,
+        },
+      });
+      if (!response.success) {
         return {
-            success: true,
+          amount: order.totalWithTax,
+          state: "Declined" as const,
+          transactionId: response.transaction.id,
+          errorMessage: response.message,
+          metadata: extractMetadataFromTransaction(response.transaction),
         };
-    },
+      }
+      return {
+        amount: order.totalWithTax,
+        state: "Authorized" as const,
+        transactionId: response.transaction.id,
+        metadata: extractMetadataFromTransaction(response.transaction),
+      };
+    } catch (e) {
+      Logger.error(e, loggerCtx);
+      return {
+        amount: order.totalWithTax,
+        state: "Declined" as const,
+        transactionId: "",
+        errorMessage: e.toString(),
+        metadata: e,
+      };
+    }
+  },
 
-    async createRefund(ctx, input, total, order, payment, args) {
-        const gateway = getGateway(args);
-        const response = await gateway.transaction.refund(payment.transactionId, (total / 100).toString(10));
-        if (!response.success) {
-            return {
-                state: 'Failed' as const,
-                transactionId: response.transaction.id,
-                metadata: response,
-            };
-        }
-        return {
-            state: 'Settled' as const,
-            transactionId: response.transaction.id,
-            metadata: response,
-        };
-    },
+  settlePayment: async (ctx, order, payment, args) => {
+    const gateway = getGateway(args);
+    try {
+      const response = await gateway.transaction.submitForSettlement(
+        payment.transactionId
+      );
+      return {
+        success: response.success,
+        errorMessage: response.errors ? response.message : undefined,
+      };
+    } catch (e) {
+      Logger.error(e, loggerCtx);
+      return { success: false, errorMessage: e.toString() };
+    }
+  },
+
+  async createRefund(ctx, input, amount, order, payment, args) {
+    const gateway = getGateway(args);
+    const response = await gateway.transaction.refund(
+      payment.transactionId,
+      (amount / 100).toString(10)
+    );
+    if (!response.success) {
+      return {
+        state: "Failed" as const,
+        transactionId: response.transaction.id,
+        metadata: response,
+      };
+    }
+    return {
+      state: "Settled" as const,
+      transactionId: response.transaction.id,
+      metadata: response,
+    };
+  },
 });
---
+++ src/plugins/braintree/index.ts
@@ -0,0 +1,1 @@
+export { BraintreePlugin } from './braintree-plugin';
