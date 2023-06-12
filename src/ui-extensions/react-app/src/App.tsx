72cd1694718ca8e704765224702c497dd61c7d61
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2023-06-12 09:06:16 +0200

    update react demo (#47)
    uses hooks and provides a table

--- src/ui-extensions/react-app/src/App.tsx
+++ src/ui-extensions/react-app/src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
-import { ProductList } from './ProductList';
+import { ProductListTable } from './ProductList';
 
 function App() {
     return (
         <div className="page-block">
             <h1>Hello from React!</h1>
-            <p>
+            <p style={{ marginBottom: '32px', maxWidth: '600px' }}>
                 This is a demonstration of how Admin UI extensions can be created using any web framework, and
                 interoperate seamlessly with the Angular-based host application.
             </p>
-            <ProductList />
+            <ProductListTable />
         </div>
     );
 }
